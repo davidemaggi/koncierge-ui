7e3d51708181bb53c635651ff3197a326023ad9e
Davide Maggi <davide.maggi@live.com>
2025-07-01 22:11:47 +0200

    Added form but need configuration

---
+++ Koncierge.Domain/DTOs/Models/CreateForwardDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Koncierge.Domain.DTOs.Models
+{
+    public class CreateForwardDto
+    {
+
+        public Guid KubeConfigId { get; set; } = Guid.Empty;
+        public string Namespace { get; set; }
+
+
+    }
+}
